c583a144406982fd1cbf82ecf2949bbebe887b7b
Matthew Dabit <mattdabit@berkeley.edu>
2015-04-27 17:15:04 -0700

    first commit

---
+++ league.py
@@ -0,0 +1,19 @@
+import random
+def makeLeague(list1, times, games, b):
+	league = []
+	int i = 0
+	int j = 0
+	int team = 0
+	for i in range(list1):
+		league.append.([list1[i], []])
+	for j in range(len(league)):
+		int x = 1
+		while (len(league[j[1]]) != games)
+			while b != 0:
+				str team1 = random.choice(list1)
+				while team1 == league[j[0]] or league[j[1]].count(team1) == times:
+					team1 = random.choice(list1)
+				league[j[1]].append(("Week" x, team1)
+				league[league[j].index(team1)[1]].append(("Week" x, team))
+				b--
+		x++
